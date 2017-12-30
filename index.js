4b26ebfa02ab31f5c593913b8a453a526186929e
Rafael Hernandez <rafa.hernandez@magnet360.com>
2017-12-29 21:20:05 -0600

    first_attempt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+const axios = require('axios');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -8,6 +9,17 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/getweather', function(request, response) {
+  axios.get('https://api.weather.gov/alerts?active=1&state=MN')
+  .then(function (response) {
+    console.log(response);
+    response.send(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+    response.send(error);
+  });
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
