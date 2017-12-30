5846666a36dc779aa0b1fe08ef0f8913d24e892b
Rafael Hernandez <rafa.hernandez@magnet360.com>
2017-12-29 22:59:28 -0600

    first_attempt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 const axios = require('axios');
 var token='';
+var weatherData = [];
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -13,6 +14,26 @@ app.get('/', function(request, response) {
 app.get('/getweather', function(request, responsefromWeb) {
   axios.get('https://api.weather.gov/alerts?active=1&state=MN')
   .then(function (response) {
+  	var datafromCall = response.data.features;
+  	for(var x=0;x<datafromCall.length;x++){
+  		var weatherItem = {
+  			"keys":{
+  				"id" : datafromCall[x].properties
+  			},
+  			"values":{
+  				"type": datafromCall[x].type,
+  				"effective": datafromCall[x].properties.type,
+  				"expires": datafromCall[x].properties.expires,
+  				"certainty": datafromCall[x].properties.certainty,
+  				"event": datafromCall[x].properties.event,
+  				"response": datafromCall[x].properties.response,
+  				"headline": datafromCall[x].properties.headline,
+  				"description": datafromCall[x].properties.description
+  			}
+  		}
+  		weatherData.push(weatherItem);
+  	}
+  	console.log(weatherData);
     responsefromWeb.send(response.data.features);
   })
   .catch(function (error) {
@@ -37,7 +58,7 @@ app.get('/connecttoMC', function(request, responsefromWeb) {
 	  .then(function(response) {
 	  	console.log(response);
 	  		responsefromWeb.send('Authorization Sent');
-	  		token = response.accessToken;
+	  		token = response.data.accessToken;
 	  	
 	}).catch(function (error) {
 	    console.log(error);
