6d50094a8c21b37ca689229c9730c3e9442e7d3d
Sunny10922 <79443223+Sunny10922@users.noreply.github.com>
2021-03-09 10:39:21 +0530

    Commit Indentation

--- index.js
+++ index.js
@@ -2,87 +2,87 @@ var express = require('express')
 var app = express()
 const axios = require('axios');
 const CircularJSON = require('circular-json');
-var token='';
+var token = '';
 var weatherData = [];
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 require('dotenv').load();
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function (request, response) {
+	response.send('Hello World!')
 })
-app.get('/getweather', function(request, responsefromWeb) {
-  axios.get('https://api.weather.gov/alerts/active/area/IL')
-  .then(function (response) {
-  	var datafromCall = response.data.features;
-  	for(var x=0;x<datafromCall.length;x++){
-  		var weatherItem = {
-  			"keys":{
-  				"theid" : datafromCall[x].properties.id
-  			},
-  			"values":{
-					"field1": datafromCall[x].type,
-					"field2": datafromCall[x].properties.sender
-  			}
-  		}
-  		weatherData.push(weatherItem);
-  	}
-    responsefromWeb.send(response.data.features);
-  })
-  .catch(function (error) {
-    console.log(error);
-    responsefromWeb.send(error);
-  });
+app.get('/getweather', function (request, responsefromWeb) {
+	axios.get('https://api.weather.gov/alerts/active/area/IL')
+		.then(function (response) {
+			var datafromCall = response.data.features;
+			for (var x = 0; x < datafromCall.length; x++) {
+				var weatherItem = {
+					"keys": {
+						"theid": datafromCall[x].properties.id
+					},
+					"values": {
+						"field1": datafromCall[x].type,
+						"field2": datafromCall[x].properties.sender
+					}
+				}
+				weatherData.push(weatherItem);
+			}
+			responsefromWeb.send(response.data.features);
+		})
+		.catch(function (error) {
+			console.log(error);
+			responsefromWeb.send(error);
+		});
 })
 
-app.get('/connecttoMC', function(request, responsefromWeb) {
+app.get('/connecttoMC', function (request, responsefromWeb) {
 	console.log(process.env.CLIENT_ID);
 	var conData = {
-	    'grant_type': 'client_credentials',	
-	    'client_id': process.env.CLIENT_ID,
-	    'client_secret': process.env.CLIENT_SECRET  
-  	}
+		'grant_type': 'client_credentials',
+		'client_id': process.env.CLIENT_ID,
+		'client_secret': process.env.CLIENT_SECRET
+	}
 	axios({
-	  method:'post',
-	  url:'https://mctg9llgcpl0dff718-t9898wqh1.auth.marketingcloudapis.com/v2/token',
-	  data: conData,
-	  headers:{
-       'Content-Type': 'application/json',
-	  }
+		method: 'post',
+		url: 'https://mctg9llgcpl0dff718-t9898wqh1.auth.marketingcloudapis.com/v2/token',
+		data: conData,
+		headers: {
+			'Content-Type': 'application/json',
+		}
 	})
-	  .then(function(response) {
+		.then(function (response) {
 			token = response.data.access_token;
 			console.log(token);
 			responsefromWeb.send('Authorization Sent');
-	  	
-	}).catch(function (error) {
-	    console.log(error);
-	    responsefromWeb.send(error);
-	  });
+
+		}).catch(function (error) {
+			console.log(error);
+			responsefromWeb.send(error);
+		});
 })
 
-app.get('/connecttoMCData', function(request, responsefromWeb) {
-	
+app.get('/connecttoMCData', function (request, responsefromWeb) {
+
 	axios({
-	    method: 'post',
-	    url: 'https://mctg9llgcpl0dff718-t9898wqh1.rest.marketingcloudapis.com/hub/v1/dataevents/key:testdataextension/rowset',
-	    data: weatherData,
-	    headers:{
-	       'Authorization': 'Bearer ' + token,
-	       'Content-Type': 'application/json',
-	    }
-	  })
-	    .then(function(response) {
-				var json = CircularJSON.stringify(response);
-	      console.log(json);
-	      responsefromWeb.send(json);
-		}) 
-		 .catch(function (error) {
+		method: 'post',
+		url: 'https://mctg9llgcpl0dff718-t9898wqh1.rest.marketingcloudapis.com/hub/v1/dataevents/key:testdataextension/rowset',
+		data: weatherData,
+		headers: {
+			'Authorization': 'Bearer ' + token,
+			'Content-Type': 'application/json',
+		}
+	})
+		.then(function (response) {
+			var json = CircularJSON.stringify(response);
+			console.log(json);
+			responsefromWeb.send(json);
+		})
+		.catch(function (error) {
 			console.log(error);
 		});
 })
- 
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+
+app.listen(app.get('port'), function () {
+	console.log("Node app is running at localhost:" + app.get('port'))
 })
--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,6 +1,6 @@
 define([
     'postmonger'
-], function(
+], function (
     Postmonger
 ) {
     'use strict';
@@ -8,7 +8,7 @@ define([
     var connection = new Postmonger.Session();
     var authTokens = {};
     var payload = {};
-    
+
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
@@ -24,7 +24,7 @@ define([
 
     }
 
-    function initialize (data) {
+    function initialize(data) {
         console.log(data);
         if (data) {
             payload = data;
@@ -40,47 +40,47 @@ define([
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
         console.log(inArguments);
 
-        $.each(inArguments, function(index, inArgument) {
-            $.each(inArgument, function(key, val) {
-                
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+
             });
         });
-        
+
         connection.trigger('updateButton', {
             button: 'next',
             text: 'done',
             visible: true
         });
 
-        
+
     }
 
-    function onGetTokens (tokens) {
+    function onGetTokens(tokens) {
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
         console.log(tokens);
         authTokens = tokens;
     }
 
-    function onGetEndpoints (endpoints) {
+    function onGetEndpoints(endpoints) {
         // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
         console.log(endpoints);
     }
 
     function save() {
-        
+
         // 'payload' is initialized on 'initActivity' above.
         // Journey Builder sends an initial payload with defaults
         // set by this activity's config.json file.  Any property
         // may be overridden as desired.
         payload.name = name;
 
-        payload['arguments'].execute.inArguments = [{ 
+        payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
             "emailAddress": "sunny.bansal@customactivity.com"
         }];
 
         payload['metaData'].isConfigured = true;
-        
+
         console.log(payload);
 
         connection.trigger('updateActivity', payload);
